8d9026027aad1316e6fd4c82f1d6d273dbdcaa1d
Ha-jeje <zagraouihajar@gmail.com>
2023-07-14 04:43:16 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,14 +4,11 @@
 /**
  * main - Entry point
  *
- * print "Programming is like building a multilingual puzzle,
- * followed by a new line"
- *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
